15c64f711bb5e617a75f4c10658fd941f1926215
Jagath <jagathmax2@gmail.com>
2019-03-11 12:51:46 +0530

    Updates on Test Class

--- src/Test.java
+++ src/Test.java
@@ -1,10 +1,17 @@
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import static javax.imageio.ImageIO.read;
 
 public class Test {
 
@@ -18,7 +25,16 @@ public class Test {
     static class MyHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange t) throws IOException {
-            String response = "This is the response";
+            InputStream inputStream = t.getRequestBody();
+            JSONParser jsonParser = new JSONParser();
+            JSONObject jsonObject = new JSONObject();
+            try {
+                jsonObject = (JSONObject)jsonParser.parse(
+                        new InputStreamReader(inputStream, "UTF-8"));
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            String response = "Payment Success for " + jsonObject.toJSONString();
             t.sendResponseHeaders(200, response.length());
             OutputStream os = t.getResponseBody();
             os.write(response.getBytes());
